436d7d18dcc384df6197501cf25a1b425878578f
Ovilia <zwl.sjtu@gmail.com>
2015-06-17 14:51:55 +0800

    set up npm, gulp and bower

---
+++ src/gulpfile.js
@@ -0,0 +1,73 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var jade        = require('gulp-jade');
+
+/**
+ * Launch the Server
+ */
+gulp.task('browser-sync', ['sass'], function() {
+    gulp.run('jade');
+    browserSync({
+        server: {
+            baseDir: '.'
+        }
+    });
+});
+
+/**
+ * Copy the bower packages
+ */
+gulp.task('vendor', function() {
+    gulp.src(['bower_components/framework7/dist/js/framework7.js*'])
+        .pipe(gulp.dest('js'));
+    gulp.src(['bower_components/framework7/dist/css/framework7.*min.css'])
+        .pipe(gulp.dest('css'));
+    gulp.src(['bower_components/framework7/dist/img/*'])
+        .pipe(gulp.dest('img/framework7'));
+});
+
+/**
+ * Compile files from _scss into css
+ */
+gulp.task('sass', function () {
+    return gulp.src('_styles/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: function(e) {
+                browserSync.notify();
+                process.stdout.write(e + '\n');
+            },
+            outputStyle: 'compressed'
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+/**
+ * Generate site using Jade
+ */
+gulp.task('jade', function() {
+    gulp.src('*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.reload({stream:true}))
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_styles/*.scss', ['sass']);
+    gulp.watch(['*.jade', 'js/*', 'css/*'], ['jade']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
